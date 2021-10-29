21b0d680eac96522afef568b5b2f9fb9ff21004e
alistairseddon <alistair.seddon@gmail.com>
2021-10-29 16:26:43 +0200

    MALDIquant workflow integration

--- Rscripts/scripts.R
+++ Rscripts/scripts.R
@@ -2,15 +2,63 @@ library("here")
 library("tidyverse")
 library("signal")
 library("mgcv")
+library("MALDIquant")
+
+
 source(here("Rscripts", "functions.R"))
-?sgolayfilt
+
 ##
 data_to_prep <- tibble(.filename = c("0035.D", "0036.D", "0037.D"))
+data_log <- read_csv2(file = here("data", "GC-MS_log.csv" ))
 
+
+#############################
 # Get Chromatogram Data
-chromatograms <- data_to_prep %>% 
-  mutate(chromatogram  = map(.x = .filename, .f = import_chromatogram_manual)) %>% 
+chromatograms <- data_log %>% 
+  mutate(chromatogram  = map(.x = Code, .f = import_chromatogram_manual)) %>% 
   unnest(cols = c(chromatogram)) 
+
+que_1 <- plot_chromatogram_gg(.x = chromatograms, facet = FALSE)
+que_1 <- plot_chromatogram_gg(.x = chromatograms, facet = TRUE)
+
+
+
+#############################
+# add in the maldi objects as column in the tibble
+chromatograms <- chromatograms %>% 
+  mutate(maldi = pmap(.l = list(.x = time,
+                                .y = intensity,
+                                GC.code = GC.code,
+                                pollen.code = pollen.code), 
+                      .f = parse_chrom_MALDI))
+
+# check working
+str(chromatograms$maldi[[3]])
+
+#############################
+# use maldiquant workflow to smooth spectra and work on baseline correction procedures
+chrom_maldi <- as.list(chromatograms$maldi)
+chrom_maldi <- transformIntensity(chrom_maldi,method="sqrt")
+
+# chrom_maldi_sg <- smoothIntensity(chrom_maldi, method="SavitzkyGolay", halfWindowSize=10)
+
+baseline <- map(.x = chrom_maldi, .f = estimateBaseline, method="SNIP", iterations=100)
+plot(chrom_maldi[[2]])
+lines(baseline[[2]], col="red", lwd=2)
+
+baseline_removed <- map(.x = chrom_maldi, .f = removeBaseline, method="SNIP", iterations=100)
+plot(baseline_removed[[3]])
+# check if this should be included ?calibrateIntensity
+
+# aligning chromatograms- does this do much?
+aligned_chromatograms <- alignSpectra(baseline_removed, halfWindowSize=20, 
+                        SNR=2, tolerance=0.002, warpingMethod="lowess")
+
+plot(aligned_chromatograms[[3]])
+
+
+
+
 
 # Do SG filter - need to look into this to understand parameter settings
 chromatograms <- chromatograms %>%
@@ -37,6 +85,16 @@ que_1
 que_1_bc <- plot_bc_chromatogram_gg(.x = baseline_corrected, facet = TRUE)
 que_1_bc
 
+
+
+
+# # standardise by the number of pollen grains in the sample
+# chromatograms <- chromatograms %>% 
+#   unnest(cols = c(intensity)) %>% 
+#   mutate(intensity =intensity/Grains ) %>% 
+#   nest(intensity = intensity)
+
+# Because of the variable number of grains we will need to standardise against a peak in the sample
 
 
 

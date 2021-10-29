ea9fe93fc7cf2a0306dee328c150369560b65933
alistairseddon <alistair.seddon@gmail.com>
2021-10-29 16:26:28 +0200

    Baseline corrected chromatogram fucntion

--- Rscripts/functions.R
+++ Rscripts/functions.R
@@ -30,13 +30,12 @@ import_chromatogram_manual <- function(.filename){
 
 ###########################
 # Plot a chromatogram using ggplot
-plot_chromatogram_gg <-function(.x = chromatograms, facet= FALSE) {
+plot_chromatogram_gg <-function(.x = chromatograms, facet= FALSE, baseline = FALSE) {
   # .x is a chromatograms tibble with nested time and intensity columns
   p <- .x %>% 
-    unnest(cols = c(time, intensity, SG)) %>% 
+    unnest(cols = c(time, intensity)) %>% 
     ggplot(aes(x = time, y = intensity, groups = pollen.code)) +
     geom_line(aes(col = pollen.code)) +
-    geom_line(aes(x = time, y = SG), col = "black") +
     xlab("Time (minutes)") +
     ylab("Intensity (m/z)") +
     theme_bw()
@@ -46,15 +45,67 @@ plot_chromatogram_gg <-function(.x = chromatograms, facet= FALSE) {
         facet_wrap(vars(pollen.code), dir = "v") +
         theme(legend.position = "none")
     }
- p 
+ 
+  if(baseline == TRUE) {
+    p <- p +  
+      geom_line(aes(x = time, y = baseline_est), col = "grey")
+  }
+  
+  p 
 }
+
+
+### parse MALDI
+
+# parse to MALDIQUANT
+
+parse_chrom_MALDI <- function(.x = chromatograms[1,]$time[[1]], .y = chromatograms[1,]$intensity[[1]],
+                              GC.code= chromatograms[1,]$GC.code,
+                              pollen.code = chromatograms[1,]$pollen.code){
+  
+  spectrum <- createMassSpectrum(mass = .x , 
+                                 intensity = .y,
+                                 metaData = list(GC.code = GC.code,
+                                                 pollen.code = pollen.code))
+  spectrum
+}
+
+
+
+
+
+
+###########################
+# Plot a baseline-corrected chromatogram using ggplot
+plot_bc_chromatogram_gg <-function(.x = baseline_corrected, 
+                                   facet= FALSE
+                                   ) {
+  # .x is a chromatograms tibble with nested time and intensity columns
+  p <- .x %>% 
+    unnest(cols = c(time, intensity, SG)) %>% 
+    ggplot(aes(x = time, y = intensity_bc, groups = pollen.code)) +
+    geom_line(aes(col = pollen.code)) +
+    xlab("Time (minutes)") +
+    ylab("Intensity (m/z)") +
+    theme_bw()
+  
+  if(facet == TRUE) {
+    p <- p +  
+      facet_wrap(vars(pollen.code), dir = "v") +
+      theme(legend.position = "none")
+  }
+  p 
+}
+
+
+
 
 
 ######
 baseline.gam <- function(.x = for_baseline_correction$time, .y = for_baseline_correction$SG){
   SG <- .y
   time <- .x
-  bs_fit <- bam(SG ~ s(time), family = Gamma()) 
+  bs_fit <- bam(SG ~ s(time, k  =15, fx = TRUE), family = Gamma()) 
   # plot(bs_fit, resid = TRUE)
   predict(bs_fit, type = "response")
 }
